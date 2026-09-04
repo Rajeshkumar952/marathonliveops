56d494ef5d8d737914d1b18ee3eaabee393a07cc
Rajesh Kumar <rajeshkumar9523rama@gmail.com>
2026-09-04 18:51:39 +0530

    Add responsive phone and laptop layout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='liveops-v6-clean-20260903';
+const CACHE='liveops-v6-responsive-approved-20260904';
 const SHELL=['./','./index.html','./styles.css','./app.js','./cloud.js','./resilience.js','./config.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil((async()=>{for(const k of await caches.keys()){if(k!==CACHE)await caches.delete(k);}await self.clients.claim();})()));
